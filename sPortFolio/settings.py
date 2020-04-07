c12277252e2392891ec01029522890a1785b7b5b
Surdyhey <sodiq.ajayi@yahoo.com>
2020-04-08 00:41:09 +0100

    Making it better

--- sPortFolio/settings.py
+++ sPortFolio/settings.py
@@ -23,7 +23,7 @@ PROJECT_ROOT = os.path.abspath(__file__)
 SECRET_KEY = 'o2z!!1rqn@!y1ci@p@_z+st_c-t*j(($+&hpk+$%i0z672xg!#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['stech.pythonanywhere.com']
 
