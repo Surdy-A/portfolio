9ad604b6e1780b8632600685846015aa2bd1e38b
Surdyhey <sodiq.ajayi@yahoo.com>
2020-04-08 00:33:00 +0100

    Making it better

--- sPortFolio/settings.py
+++ sPortFolio/settings.py
@@ -139,7 +139,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 #STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
